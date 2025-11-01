01e05157d9271522a95112022a3c98717848281d
Kuy Krawczeniuk <kjkrawczeniuk@gmail.com>
2025-11-01 14:25:22 -0700

    Write a method to insert a new item into the database

--- main.go
+++ main.go
@@ -14,12 +14,27 @@ type PantryItem struct {
 	Quantity int32
 }
 
+func (PantryItem) Create(db *sql.DB, name string) (*PantryItem, error) {
+	p := PantryItem{}
+	p.UUID = uuid.New()
+	p.Name = name
+	p.Quantity = 0
+
+	uuidBin, err := p.UUID.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = db.Exec("INSERT INTO PantryItems (uuid, name, quantity) VALUES (?, ?, ?);", uuidBin, p.Name, p.Quantity)
+	return &p, err
+}
+
 func init_db(db *sql.DB) error {
 	tx, err := db.BeginTx(context.Background(), &sql.TxOptions{})
 	if err != nil {
 		return err
 	}
-	tx.Exec("CREATE TABLE IF NOT EXISTS PantryItems (uuid BLOB PRIMARY KEY, name TEXT, quantity INTEGER);")
+	tx.Exec("CREATE TABLE IF NOT EXISTS PantryItems (uuid BLOB PRIMARY KEY, name TEXT UNIQUE, quantity INTEGER);")
 	err = tx.Commit()
 	return err
 }
